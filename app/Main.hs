4f6a289cd8fd296936ffcb4c91ca31e20add87d3
zjhmale <zjhmale@gmail.com>
2016-05-22 16:01:14 +0800

    add basic repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,53 @@
 module Main where
 
+import Eval (eval, Scope)
+import Parser (parseExpr)
+import Prologue (builtins)
+import Control.Lens
+import Control.Monad.Trans
+import System.Environment
+import System.Console.Haskeline
+import Data.List (intercalate)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Control.Exception as E
+
+process :: String -> IO ()
+process expr = E.catch (do
+                        let ast = parseExpr expr
+                        result <- eval ast builtins
+                        putStrLn $ show result)
+                       (\(E.ErrorCall e) -> do
+                        putStrLn e)
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine "λ> "
+  case minput of
+    Nothing -> do
+      outputStrLn "Goodbye."
+    Just input -> (liftIO $ process input) >> loop
+
+prologueMessage :: String
+prologueMessage = intercalate "\n"
+  ["   ___  __    ____   __",
+   "  / __)(  )  (  __) / _\\",
+   " ( (__ / (_/\\ ) _) /    \\",
+   "  \\___)\\____/(____)\\_/\\_/",
+   ""
+   ]
+
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case (args ^? element 0) of
+    Just arg -> if arg == "repl"
+               then do
+                putStrLn prologueMessage
+                runInputT defaultSettings loop
+               else do
+                file <- readFile arg
+                process file
+    Nothing -> do
+      input <- getContents
+      process input
--- src/Ast.hs
+++ src/Ast.hs
@@ -1,11 +1,15 @@
 module Ast where
 
-import Data.IORef (IORef)
+import Data.IORef (IORef, readIORef)
+import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.Map as M
 
 type Name = String
 
 newtype Fn = Fn ([SExpr] -> IO SExpr)
+
+data EnvData = EnvPair (Maybe Env, (M.Map String SExpr))
+type Env = IORef EnvData
 
 data SExpr = ENil
            | EBool Bool
@@ -18,6 +22,43 @@ data SExpr = ENil
            | EMap (M.Map String SExpr) SExpr
            | EAtom (IORef SExpr) SExpr
            | Func Fn SExpr
+           | MalFunc {fn :: Fn,
+                      ast :: SExpr,
+                      env :: Env,
+                      params :: SExpr,
+                      macro :: Bool,
+                      meta :: SExpr}
+
+stringOfList :: Bool -> String -> [SExpr] -> String
+stringOfList pr sep [] = []
+stringOfList pr sep (x:[]) = (stringOfExpr pr x)
+stringOfList pr sep (x:xs) = (stringOfExpr pr x) ++ sep ++ (stringOfList pr sep xs)
+
+flatTuples ((a,b):xs) = EString a : b : flatTuples xs
+flatTuples _          = []
+
+unescape chr = case chr of
+    '\n' -> "\\n"
+    '\\' -> "\\\\"
+    '"'  -> "\\\""
+    c    -> [c]
+
+stringOfExpr :: Bool -> SExpr -> String
+stringOfExpr _     (EKeyword key) = ":" ++ key
+stringOfExpr True  (EString str) = "\"" ++ concatMap unescape str ++ "\""
+stringOfExpr False (EString str) = str
+stringOfExpr _     (ESymbol name) = name
+stringOfExpr _     (ENum num) = show num
+stringOfExpr _     (EBool bool) = show bool
+stringOfExpr _     (ENil) = "nil"
+stringOfExpr pr    (EList items _) = "(" ++ (stringOfList pr " " items) ++ ")"
+stringOfExpr pr    (EVector items _) = "[" ++ (stringOfList pr " " items) ++ "]"
+stringOfExpr pr    (EMap m _) = "{" ++ (stringOfList pr " " (flatTuples $ M.assocs m)) ++ "}"
+stringOfExpr pr    (EAtom r _) = "(atom " ++ (stringOfExpr pr (unsafePerformIO (readIORef r))) ++ ")"
+stringOfExpr _     (Func f _) = "<fun>"
+stringOfExpr _     (MalFunc {ast=ast, env=fn_env, params=params}) = "(fn* " ++ (show params) ++ " " ++ (show ast) ++ ")"
+
+instance Show SExpr where show = stringOfExpr True
 
 mkFunc fn = Func (Fn fn) ENil
 mkfuncWithMeta fn meta = Func (Fn fn) meta
